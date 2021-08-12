22d47bb17529d6194573d962a58c0baa025b4760
leo Amato <lamato@liricus.com.ar>
2021-08-12 19:26:12 -0300

    Pasando parametros para login y singup

--- todoreminder/views.py
+++ todoreminder/views.py
@@ -2,22 +2,35 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Template, Context
 from django.shortcuts import render
 
+#AUX FUNCTIONS
+
+def create_user (first_name, last_name, username, email, password):
+    #Registrar usuario en base de datos
+    return 0
+
 def home (request):
     return render (request, 'index.html')
 
 def login (request):
     if (request.method == 'POST'):
-        print("POST METHOD")
-        return HttpResponse ("Hola")
-        # Verificar que el usuario esté registrado y mostrar el homepage
+        email = request.POST['email']
+        password = request.POST['password']
 
-    return HttpResponse("hola")
+        if (validate (email, password)):
+            return HttpResponseRedirect("")
+        else :
+            return HttpResponse("User not found")
 
 def singup (request):
     if (request.method == 'POST'):
-        print("POST METHOD")
-        return HttpResponse ("Hola")
-        # Registrar el usuario en la base de datos
-        # Redireccionar al home
-    
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        create_user (first_name, last_name, username, email, password)
+
+        return HttpResponseRedirect("/")
+
     return render (request, 'singup.html')
