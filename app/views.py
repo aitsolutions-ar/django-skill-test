3d16bcee813bd6435cf293a2ef9637b70447a1bc
lucianoinso <lucianoinso@gmail.com>
2021-08-21 23:02:14 -0300

    TASK-0: Changed project structure to fit the styleguide, added apis.py to store the APIs (instead of views.py), added directory with helper modules from the styleguide for error management.

--- app/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-
-from app import models
-from .serializers import TodoSerializer
-
-
-class TodoViewset(viewsets.ModelViewSet):
-    queryset = models.Todo.objects.all()
-    serializer_class = TodoSerializer
