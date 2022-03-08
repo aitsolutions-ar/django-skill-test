bd2fe673f48b84807620cca98dd4a277bb61d8c7
milanesio9 <milanesio2345@hotmail.com>
2022-03-07 21:01:59 -0300

    TASK-1 fix

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,15 @@
+from posixpath import basename
+from unicodedata import name
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TodoViewset
+from .views import TodoViewset, add_todo_view, todo_view
 
 router = DefaultRouter()
 
 router.register('', TodoViewset)
 
 urlpatterns = [
-    path('todos/', include(router.urls))
+    path('todos/', include(router.urls)),
+    path('list/', todo_view, name = 'Todo'),
+    path('add/', add_todo_view)
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,18 @@
 from ast import Is
+from asyncio import tasks
+from pyexpat import model
+from django.http import HttpResponseRedirect
+
+from django.shortcuts import render
+from app.forms import TodoForm
 from rest_framework import viewsets
+from .models import Todo
+
 
 from app import models
 from .serializers import TodoSerializer
 from rest_framework.permissions import IsAuthenticated
+
 
 class TodoViewset(viewsets.ModelViewSet):
     queryset = models.Todo.objects.all()
@@ -12,3 +21,22 @@ class TodoViewset(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user = self.request.user)
+    
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
+
+def todo_view(request):
+    items = models.Todo.objects.filter(user = request.user)
+    return render(request, "todo.html", {"items":items})
+
+def add_todo_view(request):
+    form = TodoForm()
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.user = request.user
+            obj.save()
+            return HttpResponseRedirect('/app/list/')
+
+    return render(request, "form.html", {'form':form})
