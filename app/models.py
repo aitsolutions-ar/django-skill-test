04620eb39d9ea4555dedad14dc68821515c4ac5e
Cristian Carballo <cristian.carballo1994@gmail.com>
2023-07-30 21:32:14 -0300

    OP-TASK-2: atributo para la asignacion de fecha límite y para poder ordenar por fecha de registro

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Todo(models.Model):
@@ -13,8 +13,13 @@ class Todo(models.Model):
     is_done = models.BooleanField(
         default=False
     )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    deadline = models.DateTimeField(
+        null=True, blank=True
+    )
     user = models.ForeignKey(
-        'auth.User',
-        on_delete=models.DO_NOTHING,
-        null=True
+        User,
+        on_delete=models.CASCADE
     )
